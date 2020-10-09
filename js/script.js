d4c70a604b554d1745049acb62d2eb41b3a38036
Dmitry Mazurkin <druglord@mail.ru>
2020-10-09 12:55:38 +0300

    Обязательное задание. Урок 2.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-let money,
-	income,
-	addExpenses,
-	deposit,
-	mission,
-	period;
+let money = 30000,
+	income = 'Фриланс',
+	addExpenses = 'Комуналка, интернет, бензин',
+	deposit = true,
+	mission = 100000,
+	period = 6;
 
+let budgetDay = money / 30;
 
-alert('Hello friends!');
-console.log('Привет из Питера');
+	// Вывод типа данных
+console.log('Тип данных переменной "money": ', typeof money);
+console.log('Тип данных переменной "income": ', typeof income);
+console.log('Тип данных переменной "deposit": ', typeof deposit);
+
+	//Вывод длины строки
+console.log('Длинная строки переменной "addExpenses": ', addExpenses.length);
+
+	//Вывод периода и цели
+console.log('Период равен ' + period + ' ' + 'месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+	// Вывод строки в нижнем регистре
+console.log('Нижний регистр: ', addExpenses.toLowerCase());
+console.log('Массив: ', addExpenses.split(', '));
+
+	// Вывод переменной budgetDay
+console.log('Дневной бюджет: ', budgetDay);
